e47ab737e3e49dfeb1c994b2b699ce5fa69672c6
Sebastián Chan <34576139+Auntex@users.noreply.github.com>
2020-11-21 02:08:51 -0300

    No se q rompi pero ya arregle el code de marco dxdxxxxxddddddddddd{

--- src/main/java/club/rigox/vanillacore/rigox/vanillacore/utils/MsgUtils.java
+++
@@ -1,9 +0,0 @@
-package club.rigox.vanillacore.rigox.vanillacore.utils;
-
-import org.bukkit.ChatColor;
-
-public class MsgUtils {
-    public static String color (String str) {
-        return ChatColor.translateAlternateColorCodes('&', str);
-    }
-}
