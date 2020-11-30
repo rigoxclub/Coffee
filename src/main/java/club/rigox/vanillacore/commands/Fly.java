2da152e6b344b04f01a45faa5dfd88f419b465dd
sebastnchan <vauntex18@gmail.com>
2020-11-30 04:17:31 -0300

    Added user with permission can set fly on other players

--- src/main/java/club/rigox/vanillacore/commands/Fly.java
+++ src/main/java/club/rigox/vanillacore/commands/Fly.java
@@ -34,6 +34,34 @@ public class Fly implements CommandExecutor {
             return true;
         }
 
+        if (args.length == 1) {
+            if (!player.hasPermission("vanillacore.fly.others")) {
+                player.sendMessage(color("&cYou don't have permission to enable fly on others."));
+                return true;
+            }
+
+            Player target = plugin.getServer().getPlayer(args[0]);
+
+            if (target == null) {
+                player.sendMessage(color("The player is not online!"));
+                return true;
+            }
+
+            if (target.equals(player)) {
+                player.sendMessage(color("&cOK WHAT THE FUCK? You can use only /FLY."));
+            }
+
+            if (target.isFlying()) {
+                flyStatus.disable(target);
+                target.sendMessage(color(String.format("&cYour fly has been deactivated by %s", player.getName())));
+                return true;
+            }
+
+            flyStatus.enable(target);
+            target.sendMessage(color(String.format("&aYour fly has been activated by %s", player.getName())));
+            return true;
+        }
+
         if (player.isFlying()) {
             flyStatus.disable(player);
             player.sendMessage(color("&cFly disabled"));
