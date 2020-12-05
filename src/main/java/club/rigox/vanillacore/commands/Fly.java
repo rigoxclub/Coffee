56553084fc3a580f02f21199e9644f6cc7ce4514
sebastnchan <vauntex18@gmail.com>
2020-12-05 16:11:26 -0300

    Less hardcoded shit for fly command

--- src/main/java/club/rigox/vanillacore/commands/Fly.java
+++ src/main/java/club/rigox/vanillacore/commands/Fly.java
@@ -27,48 +27,47 @@ public class Fly implements CommandExecutor {
 
         Player player = (Player) sender;
         if (!player.hasPermission("vanillacore.fly")) {
-            player.sendMessage(color(plugin.getLang().getString("no-permission")));
+            player.sendMessage(color(plugin.getLang().getString("permission.general-no")));
             return true;
         }
 
         if (args.length == 1) {
             if (!player.hasPermission("vanillacore.fly.others")) {
-                player.sendMessage(color("&cYou don't have permission to enable fly on others."));
+                player.sendMessage(color(plugin.getLang().getString("permission.fly-others")));
                 return true;
             }
 
             Player target = plugin.getServer().getPlayer(args[0]);
 
             if (target == null) {
-                player.sendMessage(color("The player is not online!"));
+                player.sendMessage(color(plugin.getLang().getString("player.offline")));
                 return true;
             }
 
             if (target.equals(player)) {
-                player.sendMessage(color("&cOK WHAT THE FUCK? You can use only /FLY."));
+                player.sendMessage(color(plugin.getLang().getString("fly.self")));
                 return true;
             }
 
             if (target.isFlying()) {
                 plugin.getFlyStatus().disable(target);
-                target.sendMessage(color(String.format("&cYour fly has been deactivated by %s", player.getName())));
+                target.sendMessage(color(String.format(plugin.getLang().getString("fly.disabled-other"), player.getName())));
                 return true;
             }
 
             plugin.getFlyStatus().enable(target);
-            target.sendMessage(color(String.format("&aYour fly has been activated by %s", player.getName())));
+            target.sendMessage(color(String.format(plugin.getLang().getString("fly.enabled-other"), player.getName())));
             return true;
         }
 
         if (player.isFlying()) {
             plugin.getFlyStatus().disable(player);
-            player.sendMessage(color("&cFly disabled"));
-
+            player.sendMessage(color(plugin.getLang().getString("fly.disabled")));
             return true;
         }
 
         plugin.getFlyStatus().enable(player);
-        player.sendMessage(color("&aFly enabled"));
+        player.sendMessage(color(plugin.getLang().getString("fly.enabled")));
         return false;
     }
 }
