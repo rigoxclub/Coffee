55ccf66a337db2c01cdfa49edc932c5d758dcff4
sebastnchan <vauntex18@gmail.com>
2020-12-16 13:01:10 -0300

    Gamemode command done

--- src/main/java/club/rigox/coffee/commands/admin/Gamemode.java
+++ src/main/java/club/rigox/coffee/commands/admin/Gamemode.java
@@ -28,7 +28,7 @@ public class Gamemode extends BaseCommand {
         sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode")));
     }
 
-    @Subcommand("survival")
+    @Subcommand("survival|0")
     @CommandPermission("coffee.gamemode.survival")
     @CommandCompletion("@players")
     public void survivalGamemode(CommandSender sender, String[] args) {
@@ -46,13 +46,14 @@ public class Gamemode extends BaseCommand {
             }
 
             if (target.getGameMode() == GameMode.SURVIVAL) {
-                sender.sendMessage(color(plugin.getLang().getString("gamemode.already-survival")));
+                sender.sendMessage(color(plugin.getLang().getString("gamemode.survival.other.already")));
                 return;
             }
 
-            sender.sendMessage(color(String.format("&aSuccessfully set %s to survival mode.", target.getName())));
-            target.sendMessage(color("&aYour gamemode has been set to survival!"));
+            sender.sendMessage(color(String.format(plugin.getLang().getString("gamemode.survival.other.sender"), target.getName())));
+            target.sendMessage(color(plugin.getLang().getString("gamemode.survival.set")));
             target.setGameMode(GameMode.SURVIVAL);
+            return;
         }
 
         if (!(sender instanceof Player)) {
@@ -62,11 +63,140 @@ public class Gamemode extends BaseCommand {
 
         Player player = (Player) sender;
         if (player.getGameMode() == GameMode.SURVIVAL) {
-            sender.sendMessage(color("&cYou're already in survival mode!"));
+            sender.sendMessage(color(plugin.getLang().getString("gamemode.survival.already")));
             return;
         }
 
-        player.sendMessage(color("&aYour gamemode has been updated to Survival!"));
+        player.sendMessage(color(plugin.getLang().getString("gamemode.survival.set")));
         player.setGameMode(GameMode.SURVIVAL);
+    }
+
+    @Subcommand("creative|1")
+    @CommandPermission("coffee.gamemode.creative")
+    @CommandCompletion("@players")
+    public void creativeGamemode(CommandSender sender, String[] args) {
+        if (args.length == 1) {
+            Player target = plugin.getServer().getPlayer(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(color("&cPlayer is offline!"));
+                return;
+            }
+
+            if (target.equals(sender)) {
+                sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode")));
+                return;
+            }
+
+            if (target.getGameMode() == GameMode.CREATIVE) {
+                sender.sendMessage(color(plugin.getLang().getString("gamemode.creative.other.already")));
+                return;
+            }
+
+            sender.sendMessage(color(String.format(plugin.getLang().getString("gamemode.creative.other.sender"), target.getName())));
+            target.sendMessage(color(plugin.getLang().getString("gamemode.creative.set")));
+            target.setGameMode(GameMode.CREATIVE);
+            return;
+        }
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode-others")));
+            return;
+        }
+
+        Player player = (Player) sender;
+        if (player.getGameMode() == GameMode.CREATIVE) {
+            sender.sendMessage(color(plugin.getLang().getString("gamemode.creative.already")));
+            return;
+        }
+
+        player.sendMessage(color(plugin.getLang().getString("gamemode.creative.set")));
+        player.setGameMode(GameMode.CREATIVE);
+    }
+
+    @Subcommand("adventure|2")
+    @CommandPermission("coffee.gamemode.adventure")
+    @CommandCompletion("@players")
+    public void adventureGamemode(CommandSender sender, String[] args) {
+        if (args.length == 1) {
+            Player target = plugin.getServer().getPlayer(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(color("&cPlayer is offline!"));
+                return;
+            }
+
+            if (target.equals(sender)) {
+                sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode")));
+                return;
+            }
+
+            if (target.getGameMode() == GameMode.ADVENTURE) {
+                sender.sendMessage(color(plugin.getLang().getString("gamemode.adventure.other.already")));
+                return;
+            }
+
+            sender.sendMessage(color(String.format(plugin.getLang().getString("gamemode.adventure.other.sender"), target.getName())));
+            target.sendMessage(color(plugin.getLang().getString("gamemode.adventure.set")));
+            target.setGameMode(GameMode.ADVENTURE);
+            return;
+        }
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode-others")));
+            return;
+        }
+
+        Player player = (Player) sender;
+        if (player.getGameMode() == GameMode.ADVENTURE) {
+            sender.sendMessage(color(plugin.getLang().getString("gamemode.adventure.already")));
+            return;
+        }
+
+        player.sendMessage(color(plugin.getLang().getString("gamemode.adventure.set")));
+        player.setGameMode(GameMode.ADVENTURE);
+    }
+
+    @Subcommand("spectator|3")
+    @CommandPermission("coffee.gamemode.spectator")
+    @CommandCompletion("@players")
+    public void spectatorGamemode(CommandSender sender, String[] args) {
+        if (args.length == 1) {
+            Player target = plugin.getServer().getPlayer(args[0]);
+
+            if (target == null) {
+                sender.sendMessage(color("&cPlayer is offline!"));
+                return;
+            }
+
+            if (target.equals(sender)) {
+                sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode")));
+                return;
+            }
+
+            if (target.getGameMode() == GameMode.SPECTATOR) {
+                sender.sendMessage(color(plugin.getLang().getString("gamemode.spectator.other.already")));
+                return;
+            }
+
+            sender.sendMessage(color(String.format(plugin.getLang().getString("gamemode.spectator.other.sender"), target.getName())));
+            target.sendMessage(color(plugin.getLang().getString("gamemode.spectator.set")));
+            target.setGameMode(GameMode.SPECTATOR);
+            return;
+        }
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(color(plugin.getLang().getString("command-usage.base") + plugin.getLang().getString("command-usage.gamemode-others")));
+            return;
+        }
+
+        Player player = (Player) sender;
+        if (player.getGameMode() == GameMode.SPECTATOR) {
+            sender.sendMessage(color(plugin.getLang().getString("gamemode.spectator.already")));
+            return;
+        }
+
+        player.sendMessage(color(plugin.getLang().getString("gamemode.spectator.set")));
+        player.setGameMode(GameMode.SPECTATOR);
     }
 }
