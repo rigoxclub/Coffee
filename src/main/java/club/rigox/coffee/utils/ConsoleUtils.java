d9bb8dad8a94a33b53342e2c0362b31a8e5328ec
sebastnchan <vauntex18@gmail.com>
2020-12-16 13:07:09 -0300

    Added plugin name on ConsoleUtils

--- src/main/java/club/rigox/coffee/utils/ConsoleUtils.java
+++ src/main/java/club/rigox/coffee/utils/ConsoleUtils.java
@@ -9,14 +9,14 @@ public class ConsoleUtils {
 
     public static void debug(String str) {
         if (!Coffee.instance.getSetting().getBoolean("debug")) return;
-        Bukkit.getConsoleSender().sendMessage(color(String.format("&f[&9DEBUG&f] %s", str)));
+        Bukkit.getConsoleSender().sendMessage(color(String.format("&f[&9DEBUG | Coffee&f] %s", str)));
     }
 
     public static void warn(String str) {
-        Bukkit.getConsoleSender().sendMessage(color(String.format("&f[&eWARN&f] %s", str)));
+        Bukkit.getConsoleSender().sendMessage(color(String.format("&f[&eWARN | Coffee&f] %s", str)));
     }
 
     public static void info(String str) {
-        Bukkit.getConsoleSender().sendMessage(color(String.format("&f[&aINFO&f] %s", str)));
+        Bukkit.getConsoleSender().sendMessage(color(String.format("&f[&aINFO | Coffee&f] %s", str)));
     }
 }
